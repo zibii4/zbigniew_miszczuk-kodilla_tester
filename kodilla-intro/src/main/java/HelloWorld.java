49c1162af4149f32e4b773990d207f1c64a89670
Zbigniew Miszczuk <miszczuk.zbigniew@gmail.com>
2022-04-10 01:01:36 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Zbigniew Miszczuk"); // [4]
+        System.out.println("43"); // [5]
+        System.out.println("M"); // [6]
+    }
+}

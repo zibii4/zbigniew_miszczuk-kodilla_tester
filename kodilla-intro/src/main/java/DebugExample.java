0cb606645a28c01e1d5c16ad68d5ee7e64b07a66
Zbigniew Miszczuk <miszczuk.zbigniew@gmail.com>
2022-10-20 22:49:23 +0200

    Initial version

--- kodilla-intro/src/main/java/DebugExample.java
+++ kodilla-intro/src/main/java/DebugExample.java
@@ -1,6 +1,6 @@
 public class DebugExample {
     public static void main(String[] args) {
-        int firstNumber = 22;
+        int firstNumber = 11;
         int secondNumber = 22;
 
         System.out.println(firstNumber);
